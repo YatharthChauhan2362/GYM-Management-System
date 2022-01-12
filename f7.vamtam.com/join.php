b4d9ebaa26ab2ddc0a87671317bb935c896a4782
YatharthChauhan2362 <yc232326@gmail.com>
2022-01-12 22:58:06 +0530

    join (Backend)

--- f7.vamtam.com/join.php
+++ f7.vamtam.com/join.php
@@ -1,9 +1,41 @@
 <?php
+
+
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$dob = $_POST['dob'];
+$address = $_POST['address'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zipcode = $_POST['zipcode'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+
 $connection = mysqli_connect('localhost', 'root');
 
-if ($connection) {
-    echo "Connected";
-} else {
-    echo "Error Ocurred";
+if ($connection->connect_error)
+ {
+   die('Connection Failed' -$connection->connect_error);
+} 
+else
+ {
+   $stmt=$connection->prepare("insert into registration(name, gender, dob, address, city, state, zipcode, email, phone) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+   $stmt->bind_param("ssbsssisi", $name, $gender, $dob, $address, $city, $state, $zipcode, $email, $phone);
+
+   $stmt->execute();
+
+   echo "Joined Successfully";
+
+   $stmt->close();
+   $conn->close();
+
+
 }
+
+// if ($connection) {
+//     echo "Connected";
+// } else {
+//     echo "Error Ocurred";
+// }
 ?>
