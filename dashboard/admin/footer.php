70a2ae4f262af9fb625f155af57d39fef357a00f
YatharthChauhan2362 <yc232326@gmail.com>
2022-04-06 20:36:49 +0530

    GYM

--- dashboard/admin/footer.php
+++ dashboard/admin/footer.php
@@ -1,1 +1,1 @@
-<footer class="main">Copyright &copy; 2021 <strong> Gym System </strong></footer>
+<footer class="main"><strong>Gym Management System</strong></footer>
--- index.php
+++ index.php
@@ -10,78 +10,75 @@ if(isset($_SESSION["user_data"]))
 
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>Gym | Login</title>
-	<link rel="stylesheet" href="./css/style.css"/>
-	<link rel="stylesheet" type="text/css" href="./css/entypo.css">
+    <title>Gym | Login</title>
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" type="text/css" href="./css/entypo.css">
 </head>
-<body>
 
-<body class="page-body login-page login-form-fall">
-    	<div id="container">
-			<div class="login-container">
-	
-	<div class="login-header login-caret">
-		
-		<div class="login-content">
-			
-			
-			<h1 style="color:white;">GYM System</h1>
-			
-			<!-- progress bar indicator -->
-			<div class="login-progressbar-indicator">
-				<h3>43%</h3>
-				<span>logging in...</span>
-			</div>
-		</div>
-		
-	</div>
-	
-	<div class="login-progressbar">
-		<div></div>
-	</div>
-	
-	<div class="login-form">
-		
-		<div class="login-content">
-			
-			<form action="secure_login.php" method='post' id="bb">				
-				<div class="form-group">					
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="entypo-user"></i>
-						</div>
-							<input type="text" placeholder="User ID" class="form-control" name="user_id_auth" id="textfield" data-rule-minlength="6" data-rule-required="true">
-					</div>
-				</div>				
-								
-				<div class="form-group">					
-					<div class="input-group">
-						<div class="input-group-addon">
-							<i class="entypo-key"></i>
-						</div>
-						<input type="password" name="pass_key" id="pwfield" class="form-control" data-rule-required="true" data-rule-minlength="6" placeholder="Password">
-					</div>				
-				</div>
-				
-				<div class="form-group">
-					<button type="submit" name="btnLogin" class="btn btn-primary">
-						Login In
-						<i class="entypo-login"></i>
-					</button>
-				</div>
-			</form>
-		
-				<div class="login-bottom-links">
-					<a href="forgot_password.php" class="link">Forgot your password?</a>
-				</div>			
-		</div>
-		
-	</div>
-	
-</div>
+<body style="background-image: url(images/hero-bg.jpg)">
 
-		</div>
+    <body class="page-body login-page login-form-fall">
+        <div id="container">
 
-</body>
+
+
+            <h1 style="color:white;">
+                <center>GYM MANAGEMENT SYSTEM</center>
+            </h1>
+
+
+
+            <div class="login-progressbar">
+                <div></div>
+            </div>
+
+            <br> <br> <br> <br> <br>
+            <div class="login-form">
+
+                <div class="login-content">
+
+                    <form action="secure_login.php" method='post' id="bb">
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-addon">
+                                    <i class="entypo-user"></i>
+                                </div>
+                                <input type="text" placeholder="User ID" class="form-control" name="user_id_auth"
+                                    id="textfield" data-rule-minlength="6" data-rule-required="true">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-group">
+                                <div class="input-group-addon">
+                                    <i class="entypo-key"></i>
+                                </div>
+                                <input type="password" name="pass_key" id="pwfield" class="form-control"
+                                    data-rule-required="true" data-rule-minlength="6" placeholder="Password">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <button type="submit" name="btnLogin" class="btn btn-primary">
+                                Login In
+                                <i class="entypo-login"></i>
+                            </button>
+                        </div>
+                    </form>
+
+                    <div class="login-bottom-links">
+                        <a href="forgot_password.php" class="link">Forgot your password?</a>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        </div>
+
+    </body>
+
 </html>
